e3be184f97f45a14249b1f303feb81d4e60c0670
m-glock <s0553536@htw-berlin.de>
2017-07-07 20:53:57 +0200

    animation and button of close window, except dashboard button

--- assets/UI/quit.cs
+++ assets/UI/quit.cs
@@ -4,8 +4,29 @@ using System.Collections;
 
 public class quit : MonoBehaviour{
 
+	private Animator popup;
+
 	public void QuitProgram(){
+		//pop up window
+		popup = GameObject.Find("Quit").GetComponent<Animator>();
+		popup.SetBool("quit", true);
+
+		GameObject.Find("ReturnToDashboard").GetComponent<Button>().onClick.AddListener(returnToDashboard);
+		GameObject.Find("QuitApp").GetComponent<Button>().onClick.AddListener(quitApp);
+		GameObject.Find("closeWindow").GetComponent<Button>().onClick.AddListener(closeWindow);
+	}
+
+	private void returnToDashboard(){
+
+	}
+
+	private void quitApp(){
+		//TODO maybe call save here to save data into a file? from control
+		//close the whole application
 		Application.Quit();
 	}
 
+	private void closeWindow(){
+		popup.SetBool("quit", false);
+	}
 }
